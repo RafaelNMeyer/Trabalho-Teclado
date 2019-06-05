0cc6a3784028106717eef5ab5e78ca76afe9196b
bepasar <48719874+bepasar@users.noreply.github.com>
2019-06-05 01:10:27 -0300

    classe com o método main

---
+++ Versão corrigida/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		TecladoGUI print = new TecladoGUI();
+		print.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		print.setSize(950, 700); // set frame size
+		print.setVisible(true);// display frame
+	} //
+}
